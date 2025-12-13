3711b1e38bd9f6d45b218d03c6bef62982ac33aa
kajiharuhyyy <kajiharu0921@gmail.com>
2025-12-13 22:47:59 +0900

    サービス層作成

--- src/main/java/com/kajiharuhyyy/customermanager/web/CustomerController.java
+++ src/main/java/com/kajiharuhyyy/customermanager/web/CustomerController.java
@@ -1,7 +1,5 @@
 package com.kajiharuhyyy.customermanager.web;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.kajiharuhyyy.customermanager.domain.Customer;
 import com.kajiharuhyyy.customermanager.domain.CustomerRank;
 import com.kajiharuhyyy.customermanager.domain.CustomerStatus;
-import com.kajiharuhyyy.customermanager.repo.CustomerRepository;
+import com.kajiharuhyyy.customermanager.service.CustomerService;
 import com.kajiharuhyyy.customermanager.web.form.CustomerForm;
 
 import jakarta.validation.Valid;
@@ -24,36 +22,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CustomerController {
 
-    private final CustomerRepository customerRepository;
+    //private final CustomerRepository customerService;
+    private final CustomerService customerService;
 
     @GetMapping("/customers")
     public String listCustomers(
-        @RequestParam(required = false) String keyword,
-        @RequestParam(required = false) CustomerStatus status,
-        @RequestParam(required = false) Boolean active,
-        Model model) {
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) CustomerStatus status,
+            @RequestParam(required = false) Boolean active,
+            Model model) {
         
         // 全件取得
-        List<Customer> customers = customerRepository.findAll(); 
-
-        // 名前　（部分一致）
-        if (keyword != null && !keyword.isBlank()) {
-            customers = customerRepository.findByNameContainingIgnoreCase(keyword);
-        }
-
-        // 状態
-        if (status != null) {
-            customers = customers.stream()
-                .filter(c -> c.getStatus() == status)
-                .toList();
-        }
-
-        // 有効/無効
-        if (active != null) {
-            customers = customers.stream()
-            .filter(c -> c.isActive() == active)
-            .toList(); 
-        }
+        var customers = customerService.search(keyword, status, active);
 
         model.addAttribute("customers", customers);
         model.addAttribute("statuses", CustomerStatus.values());
@@ -63,47 +43,26 @@ public class CustomerController {
         return "customers/list";
     }
 
-    @GetMapping("/customers/new")
-    public String newCustomerForm(Model model) {
-        CustomerForm form = new CustomerForm();
-        form.setActive(true);
-        model.addAttribute("customerForm", form);
-        model.addAttribute("ranks", CustomerRank.values());
-        model.addAttribute("statuses", CustomerStatus.values());
-        return "customers/new";
-    }
-
     @PostMapping("/customers")
     public String createCustomers(
             @Valid @ModelAttribute CustomerForm form,
             BindingResult bindingResult,
             Model model) {
 
-            if (bindingResult.hasErrors()) {
-                model.addAttribute("ranks", CustomerRank.values());
-                model.addAttribute("statuses", CustomerStatus.values());
-                return "customers/new";
-            }
-
-        Customer customer = Customer.builder()
-            .name(form.getName())
-            .email(form.getEmail())
-            .phone(form.getPhone())
-            .rank(form.getRank())
-            .status(form.getStatus())
-            .memo(form.getMemo())
-            .active(form.getActive())
-            .build();
-
-        customerRepository.save(customer);
-        return "redirect:/customers";	
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("ranks", CustomerRank.values());
+            model.addAttribute("statuses", CustomerStatus.values());
+            return "customers/new";
         }
+
+        customerService.create(form);
+        return "redirect:/customers";	
+    }
 
     @GetMapping("/customers/{id}/edit")
     public String editCustomerForm(@PathVariable Long id,
             Model model) {
-        Customer customer = customerRepository.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("Invalid customer ID"));
+        Customer customer = customerService.getById(id);
 
         CustomerForm form = new CustomerForm();
         form.setId(customer.getId());
@@ -126,30 +85,20 @@ public class CustomerController {
             @Valid @ModelAttribute CustomerForm form, 
             BindingResult bindingResult, 
             Model model) {
+
         if (bindingResult.hasErrors()) {
             model.addAttribute("ranks", CustomerRank.values());
             model.addAttribute("statuses", CustomerStatus.values());
             return "customers/edit";
         }
 
-        Customer customer = customerRepository.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("Invalid customer ID" + id));
-
-        customer.setName(form.getName());
-        customer.setEmail(form.getEmail());
-        customer.setPhone(form.getPhone());
-        customer.setRank(form.getRank());
-        customer.setStatus(form.getStatus());
-        customer.setMemo(form.getMemo());
-        customer.setActive(form.getActive());
-
-        customerRepository.save(customer);
+        customerService.update(id, form);
         return "redirect:/customers"; 
     }
 
     @PostMapping("/customers/{id}/delete")
     public String deleteCustomer(@PathVariable Long id) {
-        customerRepository.deleteById(id);
+        customerService.delete(id);
         return "redirect:/customers"; 
     }
 
